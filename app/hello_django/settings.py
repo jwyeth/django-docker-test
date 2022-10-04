7e63b55b37b1b43d8ac711610206e8bdd2c0636f
jwyeth <jwyeth@hotmail.com>
2022-10-04 16:26:24 -0400

    Dockerfile addition

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*efh%!4a6p5(jmbghc+ve2(m_zv8xe$0e!nivj4!n-#@-a77%*'
+SECRET_KEY = 'gHuixdoNW9tYHpiMWLHFi4xAno9HjVRz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
